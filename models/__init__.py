e53240a2847c12106c656ed051cecb7c5335dd1a
desokan <thefdmm@gmail.com>
2025-06-05 01:02:36 +0100

    updated model exports

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 # Expose models for direct import
+from .base import GeoBaseModel
 from .zone import Zone
 from .state import State
 from .lga import LGA
 from .ward import Ward
 
-__all__ = ["Zone", "State", "LGA", "Ward"]
+__all__ = ["GeoBaseModel", "Zone", "State", "LGA", "Ward"]
